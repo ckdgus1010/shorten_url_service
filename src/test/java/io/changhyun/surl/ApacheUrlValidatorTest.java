07cc91afde38d17b4d406b7bcdc4c5621bbb5eb7
ckdgus1010 <ckdgus3735@gmail.com>
2025-09-19 13:35:35 +0900

    chore: URL 검증을 위한 Apache Url Validator 의존성 추가
    - 테스트 코드 작성 후 검증이 제대로 이뤄지는지 확인

---
+++ src/test/java/io/changhyun/surl/ApacheUrlValidatorTest.java
@@ -0,0 +1,52 @@
+package io.changhyun.surl;
+
+import org.apache.commons.validator.routines.UrlValidator;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("Apache Url Validator 테스트")
+public class ApacheUrlValidatorTest {
+
+    private static final UrlValidator urlValidator = new UrlValidator(new String[]{"http", "https"});
+
+    @DisplayName("성공 케이스")
+    @Test
+    public void SuccessCases() {
+        // given
+        String[] arr = new String[]{
+                "http://example.com",
+                "http://www.example.com",
+                "https://example.com",
+                "https://www.example.com"
+        };
+
+        // when & then
+        for (String url : arr) {
+            boolean isValid = urlValidator.isValid(url);
+            assertThat(isValid).isTrue();
+        }
+    }
+
+    @DisplayName("실패 케이스")
+    @Test
+    public void FailureCases() {
+        // given
+        String[] arr = new String[]{
+                "http://",
+                "example.com",
+                "ftp://example.com",
+                "https://://example.com",
+                "https:///example.com",
+                "https:example.com",
+        };
+
+        // when & then
+        for (String url : arr) {
+            boolean isValid = urlValidator.isValid(url);
+            assertThat(isValid).isFalse();
+        }
+    }
+}
