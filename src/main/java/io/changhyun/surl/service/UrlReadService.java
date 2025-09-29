422789727715a01cda12bf739f037ccc97474e5b
ckdgus1010 <ckdgus3735@gmail.com>
2025-09-29 22:48:45 +0900

    chore: Redis, DB 조회 시 응답 속도 확인을 위한 로그 추가

--- src/main/java/io/changhyun/surl/service/UrlReadService.java
+++ src/main/java/io/changhyun/surl/service/UrlReadService.java
@@ -27,11 +27,16 @@ public class UrlReadService {
 
     @Transactional(readOnly = true)
     public String findOriginalUrl(String code) {
+        long start = System.nanoTime();
 
         // Redis 조회
         Optional<String> optional = readFromRedis(code);
         
         if (optional.isPresent()) {
+            long duration = System.nanoTime() - start;
+            String msg = String.format("Find from Redis: %.3fms", duration * 0.000001);
+            log.info(msg);
+
             return optional.get();
         }
 
@@ -40,6 +45,10 @@ public class UrlReadService {
                     log.warn("저장된 Original URL을 찾을 수 없습니다. code = {}", code);
                     return new UrlNotFoundException("URL을 찾을 수 없습니다.");
                 });
+
+        long duration = System.nanoTime() - start;
+        String msg = String.format("Find from DB: %.3fms", duration * 0.000001);
+        log.info(msg);
 
         String status = entity.getStatus().getStatusName();
 
@@ -66,8 +75,9 @@ public class UrlReadService {
     }
 
     private void saveToRedis(String code, String url, Instant expiredAt) {
+        String key = REDIS_PREFIX + code;
         Duration ttl = Duration.between(Instant.now(), expiredAt);
 
-        stringRedisTemplate.opsForValue().set(code, url, ttl);
+        stringRedisTemplate.opsForValue().set(key, url, ttl);
     }
 }
